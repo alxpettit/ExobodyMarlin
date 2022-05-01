a688c80f52882520cd09565ab0e071817bbf70de
Alexandria P <alxpettit@gmail.com>
2022-04-30 21:12:24 -0700

    improved script for pushing code

--- push.sh
+++ push.sh
@@ -1,13 +1,60 @@
 #!/usr/bin/env bash
 
-target_part="/dev/disk/by-id/usb-Generic_MassStorageClass_000000001536-0:0-part1"
+# VERY IMPORTANT! Strict mode. See: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
 
-ssh root@yuri-mech mkdir -p /mnt/target
-ssh root@yuri-mech mount "$target_part" /mnt/target
-ssh root@yuri-mech rm /mnt/target/*.bin
-rsync -P $(echo .pio/build/STM32F103RET6_creality/firmware-*.bin | choose -1) root@yuri-mech:/mnt/target/
-while true; do
-ssh root@yuri-mech umount "$target_part" || exit
-done
-# ssh root@yuri-mech fsck "$target_part"
+source push.conf
 
+# configs
+: "${control_path_prefix:=${HOME}/.ssh/ctl}"
+: "${control_path:=${control_path_prefix}/%L-%r@%h:%p}"
+: "${build_env:=STM32F103RET6_creality}"
+: "${remote_mountpoint:=/mnt/target}"
+: "${bin_path:=.pio/build/${build_env}}"
+: "${target_partition:=}"
+: "${host:=}"
+
+function check_exists() {
+  var_name="$1"
+  var_val="$2"
+
+  if [ -z "${var_val}" ]; then
+    echo "\"${var_name}\" is not defined" 1>&2
+    exit 1
+  fi
+}
+
+check_exists target_partition "${target_partition}"
+check_exists host "${host}"
+
+# make sure this path exists
+mkdir -p "${control_path_prefix}"
+
+# open SSH session
+ssh -nNf -o ControlMaster=yes -o ControlPath="${control_path}" "${host}"
+
+
+# shellcheck disable=SC2154 # ${target_partition} is specified in push.conf
+# shellcheck disable=SC2087 # local expansion is intended behavior
+ssh root@yuri-mech bash << EOF
+  mkdir -p "${remote_mountpoint}" && \
+  mount "${target_partition}" "${remote_mountpoint}" && \
+  rm "${remote_mountpoint}"/*.bin
+EOF
+
+latest_firmware=$(echo "${bin_path}/firmware-"*.bin | choose -1)
+
+rsync -e "ssh -o ControlPath='${control_path}'" -P "${latest_firmware}"  "${host}":/mnt/target/
+
+# shellcheck disable=SC2087 # local expansion is intended behavior
+ssh "${host}" bash << EOF
+  while true; do
+    umount "${target_partition}" || exit
+  done
+EOF
+
+# ssh root@yuri-mech fsck "$target_partition"
+
+# Close SSH session
+ssh -O exit -o ControlPath="${control_path}"  "${host}"
