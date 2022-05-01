d8c0e1f759b147f5e424b4933a49303b7238c397
Alexandria P <alxpettit@gmail.com>
2022-04-30 21:40:46 -0700

    hate u all

--- push.sh
+++ push.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
+# I HATE BASH
+# BASH IS TRASH
+# FUCK YOU ALL
+# -Alexandria Pettit, 2022
+# GNU GPLv3
+# FISH shell people, pls make a linter u dummies
+
 # VERY IMPORTANT! Strict mode. See: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+shopt -s nullglob
 set -euo pipefail
 IFS=$'\n\t'
 
+echo "Loading config..."
 source push.conf
 
 # configs
 : "${control_path_prefix:=${HOME}/.ssh/ctl}"
-: "${control_path:=${control_path_prefix}/%L-%r@%h:%p}"
+: "${control_path:=${control_path_prefix}/push-%L-%r@%h:%p}"
 : "${build_env:=STM32F103RET6_creality}"
 : "${remote_mountpoint:=/mnt/target}"
 : "${bin_path:=.pio/build/${build_env}}"
 : "${target_partition:=}"
 : "${host:=}"
 
+echo "Configs loaded."
+
 latest_firmware=$(echo "${bin_path}/firmware-"*.bin | choose -1)
+echo "I believe the latest firmware is: ${latest_firmware}"
 
 function check_exists() {
   var_name="$1"
@@ -30,27 +42,45 @@ function check_exists() {
 check_exists target_partition "${target_partition}"
 check_exists host "${host}"
 
-# make sure this path exists
-mkdir -p "${control_path_prefix}"
+echo "Making sure control path prefix exists..."
+mkdir -vp "${control_path_prefix}"
 
-# open SSH session
+echo "Closing old SSH sessions..."
+old_session_files=( "${control_path_prefix}/push-"* )
+
+if (( ${#old_session_files[@]} )); then
+  for session in "${old_session_files[@]}"; do
+    echo "Old session detected: ${session}"
+    ssh -O exit -o ControlPath="${control_path}"  "${host}" || {
+      echo "Exit of old session failed. Oh well."
+    }
+  done
+else
+  echo "No old sessions exist. Yay!"
+fi
+
+echo "Opening SSH session..."
 ssh -nNf -o ControlMaster=yes -o ControlPath="${control_path}" "${host}"
 
 ssh root@yuri-mech bash << EOF
-  mkdir -p "${remote_mountpoint}" && \
+  mkdir -p "${remote_mountpoint}"
+  umount "${target_partition}"
   mount "${target_partition}" "${remote_mountpoint}" && \
-  rm "${remote_mountpoint}"/*.bin
+  rm -fv "${remote_mountpoint}"/*.bin
 EOF
 
-
+echo "Copying file to remote microSD card..."
 rsync -e "ssh -o ControlPath='${control_path}'" -P "${latest_firmware}"  "${host}":/mnt/target/
 
 # shellcheck disable=SC2087 # local expansion is intended behavior
 ssh "${host}" bash << EOF
   while true; do
-    umount "${target_partition}" || exit
+    umount "${target_partition}" || {
+      echo "Unmount of remote partition failed. Mountpoints must be exhausted. This is normal."
+      break
+    }
   done
 EOF
 
-# Close SSH session
+echo "Closing SSH session..."
 ssh -O exit -o ControlPath="${control_path}"  "${host}"
